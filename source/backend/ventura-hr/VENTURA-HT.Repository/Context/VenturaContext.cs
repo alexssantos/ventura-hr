13ae714c407fae2797aa41170eb0b5b483ec621c
Alex Santos <arkanjo.alex@gmail.com>
2020-10-07 01:12:12 -0300

    REpositories Candidato, Empresa, Admin Domain Repository interfaces Base Repository

---
+++ source/backend/ventura-hr/VENTURA-HT.Repository/Context/VenturaContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VENTURA_HR.DOMAIN.UsuarioAggregate.Entities;
+using VENTURA_HT.Repository.MapEntities;
+
+namespace VENTURA_HT.Repository.Context
+{
+	public class VenturaContext : DbContext
+	{
+		public VenturaContext(DbContextOptions<VenturaContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Usuario> Usuarios { get; set; }
+		public DbSet<Candidato> Candidatos { get; set; }
+		public DbSet<Empresa> Empresas { get; set; }
+		public DbSet<Administrador> Administradores { get; set; }
+
+
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfiguration(new UsuarioMap());
+			modelBuilder.ApplyConfiguration(new CandidatoMap());
+			modelBuilder.ApplyConfiguration(new EmpresaMap());
+
+
+			// Mapping the Table-Per-Type (TPT) Inheritance
+			//modelBuilder.Entity<Empresa>().ToTable("Empresa");
+
+
+			base.OnModelCreating(modelBuilder);
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+		}
+	}
+}
---
+++ source/backend/ventura-hr/VENTURA-HT.Repository/MapEntities/EmpresaMap.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VENTURA_HR.DOMAIN.UsuarioAggregate.Entities;
+
+namespace VENTURA_HT.Repository.MapEntities
+{
+	public class EmpresaMap : IEntityTypeConfiguration<Empresa>
+	{
+		public void Configure(EntityTypeBuilder<Empresa> builder)
+		{
+			builder.ToTable("Empresa");
+		}
+	}
+}

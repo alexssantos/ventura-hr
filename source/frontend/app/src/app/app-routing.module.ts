593c3017866a047b20d59d60fd457f7557107e3b
Alex Santos <arkanjo.alex@gmail.com>
2020-10-25 12:59:48 -0300

    Estrutura basica com HOme page

--- source/frontend/app/src/app/app-routing.module.ts
+++ source/frontend/app/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SignInUpComponent } from './pages/sign-in-up/sign-in-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'access', component: SignInUpComponent },
+	{ path: 'home', component: HomeComponent },
+	//{ path: 'notfound', component: PageNotFoundComponent },	
+	{
+		path: 'profile', redirectTo: 'compare', pathMatch: 'full'
+	},	
+	{
+		path: '', redirectTo: '/home', pathMatch: 'full'
+	},	
+	{
+		//entra NotFoundCOmponent
+		path: '**', redirectTo: "/home"
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

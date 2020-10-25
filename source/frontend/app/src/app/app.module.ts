9ae3a671c83847f2ed75bfb6d5036b3d20302f3e
Alex Santos <arkanjo.alex@gmail.com>
2020-10-24 22:32:06 -0300

    Criado componentes da pagina home - job-card, search-bar, filter-box

--- source/frontend/app/src/app/app.module.ts
+++ source/frontend/app/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SignInUpComponent } from './pages/sign-in-up/sign-in-up.component';
 import { HeaderComponent } from './theme/components/header/header.component';
+import { FilterBoxComponent } from './theme/components/filter-box/filter-box.component';
+import { SearchBarComponent } from './theme/components/search-bar/search-bar.component';
+import { JobCardComponent } from './theme/components/job-card/job-card.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HomeComponent,
 		SignInUpComponent,
-		HeaderComponent
+		HeaderComponent,
+		FilterBoxComponent,
+		SearchBarComponent,
+		JobCardComponent
 	],
 	imports: [
 		BrowserModule,
